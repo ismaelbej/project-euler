22813b859f8a6c6330bf73b8847b50d43ad68d2c
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-08-22 23:40:45 +0000

    Problema 2.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@14 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P2.scala
+++ src/ismael/P2.scala
@@ -1,10 +1,17 @@
 package ismael
 
-object P2 {
-	def genFibonacci() = {
-		
+object P2 {	
+	def genFibLessThan(N: Int): List[Int] = {
+		def genFibSeq(a: Int, b:Int): List[Int] = {
+			if (a > N) Nil
+			else a :: genFibSeq(b, a+b)
+		}
+		genFibSeq(1, 1)
+	}
+	def filterSeq(seq: List[Int]): List[Int] = {
+		for (n <- seq if n%2 == 0) yield n
 	}
 	def run(args: Array[String]): Unit = {
-		
+		println((0 /: filterSeq(genFibLessThan(4000000))) {_ + _})
 	}
 }
