e4a756bd8ad9abf50ea6f9e10c2443ad9f1e5936
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-08-29 22:50:27 +0000

    Problema 33.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@32 8898f3a5-19f9-486b-9892-a321095f26e3

---
+++ src/ismael/P33.scala
@@ -0,0 +1,22 @@
+package ismael
+
+object P33 {
+	def isEquivFrac(a: Int, b: Int, d: Int): Boolean = {
+		val ab = 10*a+b
+		val bd = 10*b+d
+		ab*d == bd*a
+	}
+	def isNonTrivialFrac(a: Int, d:Int): Boolean = {
+		!(for (b <- 1 to 9 if isEquivFrac(a, b, d)) yield b).isEmpty
+	}
+	def dcm(n: Int, d: Int) : Int = {
+		if (d==0) n
+		else dcm(d, n%d)
+	}
+	def run(args: Array[String]): Unit = {
+		val frac = for (a <- 1 to 8; d <- a+1 to 9 if isNonTrivialFrac(a, d)) yield (a,d)
+		val (num, denom) = ((1,1) /: frac) {(x, y) => (x._1* y._1, x._2*y._2)}
+		val d = dcm(num, denom)
+		println(num, denom, denom/d)
+	}
+}
