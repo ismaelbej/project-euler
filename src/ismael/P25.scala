07ee18ba110d8726c44e06e7d561f6f61857278d
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-05 21:38:39 +0000

    Hago que sea mas 'programacion funcional'.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@49 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P25.scala
+++ src/ismael/P25.scala
@@ -2,24 +2,18 @@ package ismael
 
 object P25 {
 	val digits = 1000
-	def run(args: Array[String]): Unit = {
-		var a = 1.0
-		var b = 1.0
-		var c = 1
-		var n = 1
-		while (c < digits) 
-		{
-			var t = b  
-			b += a
-			a = t
-			if (a >= 10.0)
-			{
-				a /= 10.0
-				b /= 10.0
-				c += 1
-			}
-			n += 1
+	def sumFib(p: ((Int, Double, Int), (Int, Double, Int))): (Int, Double, Int) = {
+		val x = p._1
+		val y = p._2
+		if (x._1 == y._1) {
+			val s = x._2 + y._2
+			if (s >= 10.0) (x._1 + 1, s/10.0, y._3 + 1)
+			else (x._1, s, y._3 + 1)
 		}
-		println(n)
+		else (y._1, x._2/10.0 + y._2, y._3 + 1)
+	}
+	val fibDigits: Stream[(Int, Double, Int)] = (1, 1.0, 1) #:: (1, 1.0, 2) #:: fibDigits.zip(fibDigits.tail).map(sumFib)
+	def run(args: Array[String]): Unit = {
+		println((fibDigits dropWhile {_._1 < digits}).head._3)
 	}
 }
