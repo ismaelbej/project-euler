881fac6d6338c2c663720110ca4c3ff207ed6811
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-05 21:56:45 +0000

    No hace falta que guarde la posicion dentro de la serie de Fibonacci.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@50 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P25.scala
+++ src/ismael/P25.scala
@@ -2,18 +2,18 @@ package ismael
 
 object P25 {
 	val digits = 1000
-	def sumFib(p: ((Int, Double, Int), (Int, Double, Int))): (Int, Double, Int) = {
+	def sumFib(p: ((Int, Double), (Int, Double))): (Int, Double) = {
 		val x = p._1
 		val y = p._2
 		if (x._1 == y._1) {
 			val s = x._2 + y._2
-			if (s >= 10.0) (x._1 + 1, s/10.0, y._3 + 1)
-			else (x._1, s, y._3 + 1)
+			if (s >= 10.0) (x._1 + 1, s/10.0)
+			else (x._1, s)
 		}
-		else (y._1, x._2/10.0 + y._2, y._3 + 1)
+		else (y._1, x._2/10.0 + y._2)
 	}
-	val fibDigits: Stream[(Int, Double, Int)] = (1, 1.0, 1) #:: (1, 1.0, 2) #:: fibDigits.zip(fibDigits.tail).map(sumFib)
+	val fibDigits: Stream[(Int, Double)] = (1, 1.0) #:: (1, 1.0) #:: fibDigits.zip(fibDigits.tail).map(sumFib)
 	def run(args: Array[String]): Unit = {
-		println((fibDigits dropWhile {_._1 < digits}).head._3)
+		println(1 + (fibDigits takeWhile {_._1 < digits} length))
 	}
 }
