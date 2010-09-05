29b25f41edb57fbede84f7f91a0a2252878d9ecb
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-05 21:15:17 +0000

    Reemplazo el uso de Lista con streams para generar los numberos de Fibonacci.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@48 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P2.scala
+++ src/ismael/P2.scala
@@ -1,15 +1,9 @@
 package ismael
 
-object P2 {	
-	def genFibLessThan(N: Int): List[Int] = {
-		def genFibSeq(a: Int, b:Int): List[Int] = {
-			if (a > N) Nil
-			else a :: genFibSeq(b, a+b)
-		}
-		genFibSeq(1, 1)
-	}
+object P2 {
 	val Max = 4000000
-	val evenFib = genFibLessThan(Max) filter {_%2 == 0}
+	val fib: Stream[Int] = 1 #:: 1 #:: fib.zip(fib.tail).map(p => p._1 + p._2)
+	val evenFib = fib takeWhile {_<Max} filter {_%2 == 0}
 	def run(args: Array[String]): Unit = {
 		println((0 /: evenFib) {_ + _})
 	}
