ef6013b6390e2d67ea2b3bf5aca69b8c4bc57bc1
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-06 00:05:55 +0000

    Simplifico la operacion power.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@52 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P16.scala
+++ src/ismael/P16.scala
@@ -1,14 +1,8 @@
 package ismael
 
 object P16 {
-	val number = power(2, 1000)
-	def power(a: BigInt, n: Int): BigInt = {
-		def powerRec(p: BigInt, n: Int): BigInt = {
-			if (n==0) p
-			else powerRec(p*a, n-1)
-		}
-		powerRec(1, n)
-	}
+	val number = power(BigInt(2), 1000)
+	def power(a: BigInt, n: Int): BigInt = (BigInt(1) /: List.fill(n)(a)) {_*_}
 	def sumDigits(n: BigInt): BigInt = {
 		def sumDigitsRec(s: BigInt, n: BigInt): BigInt = {
 			if (n == 0) s
