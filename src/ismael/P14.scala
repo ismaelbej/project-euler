0c3a408c6d19b84381712f1a7fbbe57aceb065e3
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-06 05:40:10 +0000

    Cambiar for 'imperativo' por una operacion sobre listas, mas 'funcional'.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@56 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P14.scala
+++ src/ismael/P14.scala
@@ -4,10 +4,10 @@ package ismael
 object P14 {
 	val N = 1000000
 	import scala.collection.mutable
-	val vals = mutable.Map.empty[Long, Long]
-	def collatz(n: Long): Long = {
-		def collatzRec(n: Long, c: Long): Long = {
-			if (vals.contains(n)) c+vals(n)
+	val values = mutable.Map.empty[Int, Int]
+	def collatz(n: Int): Int = {
+		def collatzRec(n: Long, c: Int): Int = {
+			if (values.contains(n.toInt)) c+values(n.toInt)
 			else
 			{
 				if (n == 1) c
@@ -16,20 +16,15 @@ object P14 {
 			}
 		}
 		val r = collatzRec(n, 0)
-		vals + ((n, r))
+		values + ((n, r))
 		r
 	}
 	def run(args: Array[String]): Unit = {
-		var max = 0L
-		var pos = 0
-		for (x <- 1 to N) 
-		{
-			if (collatz(x) > max)
-			{
-				max = collatz(x)
-				pos = x
-			}
-		}
-		println(pos)
+		val s = (1 to N) map {x => (collatz(x), x)}
+		val m = s.foldLeft((0, 0))((a, b) => {
+			if (b._1 > a._1) b
+			else a
+		})
+		println(m._2)
 	}
 }
