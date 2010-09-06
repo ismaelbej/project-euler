755590e10b10740182ab6955cf299cb96d55c04c
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-06 00:06:32 +0000

    Corrijo detalles de estilo.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@53 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P18.scala
+++ src/ismael/P18.scala
@@ -16,9 +16,7 @@ object P18 {
 91 71 52 38 17 14 91 43 58 50 27 29 48
 63 66 04 68 89 53 67 30 73 16 69 87 40 31
 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
-	def makePyramid(s: String): Array[Array[Int]] = {
-		s.split('\n') map {_.split(' ') map {_.toInt}}
-	}
+	def makePyramid(s: String): Array[Array[Int]] = s.split('\n') map {_.split(' ') map {_.toInt}}
 	val pyramid = makePyramid(pyramidString)
 	def sumPos(t: Array[Int], b: Array[Int], n: Int): Int = {
 		if (n == 1) b(n-1)+t(0)
@@ -33,7 +31,7 @@ object P18 {
 			if (a.isEmpty) h
 			else addPyramidRows(addRows(h, a.head), a.tail)
 		}
-		addPyramidRows(a(0), a.tail) max
+		addPyramidRows(a head, a tail) max
 	}
 	def run(args: Array[String]): Unit = {
 		println(findMaxPyramid(pyramid))
