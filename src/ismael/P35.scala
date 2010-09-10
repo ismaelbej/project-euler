9b2dc87bf638189f1ddc432b54a033a850446707
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-10 03:42:56 +0000

    Simplificar la funcion isPrime.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@61 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P35.scala
+++ src/ismael/P35.scala
@@ -1,16 +1,16 @@
 package ismael
 
 object P35 {
-	def sq(j: Int)(k: Int): Boolean = k*k <= j
-	def isPrime(j: Int): Boolean = {
-		if (j >= 0) primes.takeWhile(sq(j)).forall(j%_!=0)
-		else primes.takeWhile(sq(-j)).forall(j%_!=0)
-	}
+	val Max = 1000000
+	def isPrime(j: Int): Boolean = primes takeWhile {x => x*x <= j} forall {j%_!=0}
 	def nextPrime(i: Int): Int = Stream.from(i + 1).find(isPrime).get
 	val primes: Stream[Int] = 2 #:: primes.map(nextPrime)
-	val Max = 1000000
 	def power(b: Int, e: Int): Int = (1 /: List.fill(e)(b)) {_*_}
 	def isCircular(p: Int): Boolean = {
+		def countDigits(p: Int): Int = p match {
+			case 0 => 0
+			case _ => 1 + countDigits(p/10)
+		}
 		val l = countDigits(p)
 		def rotateLeft(d: Int): Int = {
 			val b = power(10,l-d)
@@ -18,14 +18,10 @@ object P35 {
 			val a = p/b
 			(p-a*b)*c+a
 		}
-		(for (n <- 1 to l) yield rotateLeft(n)).forall(isPrime)
-	}
-	def countDigits(p: Int): Int = p match {
-		case 0 => 0
-		case _ => 1 + countDigits(p/10)
+		1 to l map rotateLeft forall isPrime
 	}
 	val circularPrimes = primes takeWhile {_<Max} filter isCircular
 	def run(args: Array[String]): Unit = {
-		println(circularPrimes.length)
+		println(circularPrimes length)
 	}
 }
