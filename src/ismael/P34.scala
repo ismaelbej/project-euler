15d3cf2a83a5d79e518f26fd3b2765d6d8f187a4
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-11 17:03:36 +0000

    Problema 34, muy similar al 30.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@66 8898f3a5-19f9-486b-9892-a321095f26e3

---
+++ src/ismael/P34.scala
@@ -0,0 +1,31 @@
+package ismael
+
+object P34 {
+	def fact(b: Int): Int = (1 /: List.range(1, b+1)) {_*_}
+	def getFactSum(l: List[Int]): Int = l.foldLeft(0)((x, y) => x+fact(y))
+	def getDigits(n: Int): List[Int] = n match {
+		case 0 => Nil
+		case _ => n%10 :: getDigits(n/10)
+	}
+	def hasDigits(n: Int, l: List[Int]): Boolean = getDigits(n).sort(_>_) == l
+	def isFactSum(l: List[Int]): Boolean = hasDigits(getFactSum(l), l)
+	def nextSequenceIncr(l: List[Int]): List[Int] = l match {
+		case Nil => List(0)
+		case _ => nextSequence(l)
+	}
+	def nextSequence(l: List[Int]): List[Int] = l match {
+		case h :: t => {
+			if (h < 9) (h+1) :: t
+			else {
+				val r = nextSequenceIncr(t)
+				r.head :: r
+			}
+		}
+		case _ => Nil
+	}
+	val sequences: Stream[List[Int]] = List(0, 1) #:: sequences.map(nextSequence)
+	val validNumbers = sequences takeWhile(_.length <= 7) filter isFactSum map getFactSum
+	def run(args: Array[String]): Unit = {
+		println(validNumbers.sum)
+	}
+}
