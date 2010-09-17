5e069b11c4b4d04fb578bf37cceca26023a46ce7
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-17 04:11:16 +0000

    Actualizo P10 para que utilice la libreria ProjectEuler un algoritmo mas funcional.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@75 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P10.scala
+++ src/ismael/P10.scala
@@ -1,19 +1,9 @@
 package ismael
 
 object P10 {
-	val N = 2000000
-	val sieve = new Array[Boolean](N)
-	def run(args: Array[String]): Unit = {
-		var n: Int = 2
-		while (n*n <= N)
-		{
-			if (!sieve(n))
-			{
-				for (k <- n to N/n if k*n < N) sieve(k*n) = true
-			}
-			n+=1
-		}
-		val primes = for (n <- 2 to N-1 if !sieve(n)) yield n
-		println((0L /: primes) {_+_})
-	}
+  val N = 2000000
+  val primesBelowN = ProjectEuler.primes takeWhile {_<=N} map {_.toLong}
+  def run(args: Array[String]): Unit = {
+    println(primesBelowN sum)
+  }
 }
