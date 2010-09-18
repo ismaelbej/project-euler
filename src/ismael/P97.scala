5459b08039237fb624f034a655366f1c03958ebe
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-18 23:20:03 +0000

    Modifico el problema 97 para mover la definicion de powerMod a la libreria.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@79 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P97.scala
+++ src/ismael/P97.scala
@@ -8,22 +8,6 @@ object P97 {
   val ex = 7830457
   val M = power(10L, 10)
   val Num = (A * powerMod(b, ex, M) + 1) % M
-  def powerMod(b: Long, ex: Long, M: Long): Long = {
-    def powerModRec(p: Long, b: Long, ex: Long): Long = ex match {
-      case 0 => p
-      case _ => {
-        val bb = (BigInt(b) * b) % M        
-        if ((ex&1)==0) {
-          powerModRec(p, bb toLong, ex/2)
-        }
-        else {
-          val pb = (BigInt(p) * b) % M
-          powerModRec(pb toLong, bb toLong, ex/2)
-        }
-      }
-    }
-    powerModRec(1, b%M, ex) 
-  }
   def run(args: Array[String]): Unit = {
     println(Num)
   }
