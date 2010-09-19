0efbc570556ac08a951b3ae4ce8ae9d54de24fad
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-19 00:31:59 +0000

    Problema 12: * Cambio indentacion. * Hago un mejor uso de los Stream. * Optimizo el calculo de los divisores de un numero triangular.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@81 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P12.scala
+++ src/ismael/P12.scala
@@ -1,32 +1,37 @@
 package ismael
 
+import ProjectEuler._
+
 object P12 {
-	def power(a: Int, n: Int): Int = (1 /: List.fill(n)(a)) {_*_}
-	def countMaxDiv(n: Int, d: Int): Int = {
-		def countMaxDivRec(n: Int, c: Int): Int = {
-			if (n%d != 0) c
-			else countMaxDivRec(n/d, c+1)
-		}
-		countMaxDivRec(n, 0)
-	}
-	def countDiv(n: Int): Int = {
-		def countDivRec(n: Int, p: Int, d: Int): Int = {
-			if (n == 1) p
-			else if (d*d > n) 2*p
-			else
-			{
-				val k = countMaxDiv(n, d)
-				val m = power(d, k)
-				countDivRec(n/m, p*(k+1), d+1)
-			}
-		}
-		countDivRec(n, 1, 2)
-	}
-	def triangleNumber(n: Int): Int = n*(n+1)/2
-	def makeTriangleNumbers(s: Stream[Int]): Stream[Int] = Stream.cons(triangleNumber(s head), makeTriangleNumbers(s tail));
-	val naturalNumbers: Stream[Int] = Stream.cons(1, naturalNumbers map {_+1}) 
-	val triangleNumbers = Stream.cons(1, makeTriangleNumbers(naturalNumbers tail))
-	def run(args: Array[String]): Unit = {
-		println(triangleNumbers filter {countDiv(_) > 500} head)
-	}
+  def reduceDiv(n: Int, d: Int): (Int, Int) = {
+    def reduceDivRec(n: Int, c: Int): (Int, Int) = {
+      if (n%d != 0) (c, n)
+      else reduceDivRec(n/d, c+1)
+    }
+    reduceDivRec(n, 0)
+  }
+  def countDiv(n: Int): Int = {
+    def countDivRec(n: Int, p: Int, d: Int): Int = {
+      if (n == 1) p
+      else if (d*d > n) 2*p
+      else {
+	val (c, m) = reduceDiv(n, d)
+	countDivRec(m, p * (c + 1), d + 1)
+      }
+    }
+    countDivRec(n, 1, 2)
+  }
+  def countTriangleNumberDiv(n: Int): Int = {
+    if ((n&1) == 0) {
+      countDiv(n / 2) * countDiv(n + 1)
+    }
+    else {
+      countDiv(n) * countDiv((n + 1) / 2)
+    }
+  }
+  def makeTriangleNumber(n: Int): Int = n * (n + 1) / 2
+  val triangleNumbersFiveHundDiv = Stream.from(1) filter {countTriangleNumberDiv(_) > 500}
+  def run(args: Array[String]): Unit = {
+    println(makeTriangleNumber(triangleNumbersFiveHundDiv head))
+  }
 }
