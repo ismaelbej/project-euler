536e00c8f62cd222fff3dae04801cdc08dfc6b6e
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-19 19:49:34 +0000

    Problema 48: Cambio un poco el estilo.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@90 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P48.scala
+++ src/ismael/P48.scala
@@ -4,12 +4,9 @@ import ProjectEuler._
 
 object P48 {
   val M = power(10L, 10)
-  def sumMod(a: Long, b: Long, M: Long): Long = {
-    val s = BigInt(a)
-    ((s + b) % M) toLong
-  }
-  val powerSequence = List.range(1, 1001) map {x => powerMod(x, x, M)}
-  val sumPowerSequence = powerSequence.foldLeft(0L)((x, y) => sumMod(x, y, M))
+  def sumMod(a: Long, b: Long, M: Long): Long = ((BigInt(a) + b) % M) toLong
+  val powerSequence = 1 to 1000 map {x => powerMod(x, x, M)}
+  val sumPowerSequence = powerSequence.foldLeft(0L) {(x, y) => sumMod(x, y, M)}
   def run(args: Array[String]): Unit = {
     println(sumPowerSequence)
   }
