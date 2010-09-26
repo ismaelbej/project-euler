d230a6acd3c65bfc1e7cf03d70a9c544252db57a
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-26 04:30:29 +0000

    Problema 46: Corregir el estilo, y utilizacion mejor de un Stream.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@97 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P46.scala
+++ src/ismael/P46.scala
@@ -3,9 +3,9 @@ package ismael
 import ProjectEuler._
 
 object P46 {
-  val odds = Stream.from(0) map {2*_ + 1}
+  val odds = Stream.from(1, 2)
   val squares = Stream.from(0) map {x => x*x}
-  def isGoldbach(n: Int): Boolean = squares.takeWhile(y => 2*y < n).exists(y => isPrime(n - 2*y))
+  def isGoldbach(n: Int): Boolean = squares takeWhile {y => 2*y < n} exists {y => isPrime(n - 2*y)}
   val nonGoldbach = odds filterNot isGoldbach
   def run(args: Array[String]): Unit = {
     println(nonGoldbach head)
