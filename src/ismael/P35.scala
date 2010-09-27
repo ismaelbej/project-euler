89c8589a790e5f6e61dc9818047824092490acce
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-27 03:36:24 +0000

    Problema 35: Correccion de estilo.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@104 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P35.scala
+++ src/ismael/P35.scala
@@ -7,14 +7,14 @@ object P35 {
   def isCircular(p: Int): Boolean = {
     val l = countDigits(p)
     def rotateLeft(d: Int): Int = {
-      val b = power(10, l-d)
+      val b = power(10, l - d)
       val c = power(10, d)
       val a = p/b
-      (p-a*b)*c+a
+      (p - a * b) * c + a
     }
-    (1 to l-1) forall {x => isPrime(rotateLeft(x))}
+    (1 to l - 1) forall {x => isPrime(rotateLeft(x))}
   }
-  val circularPrimes = primes takeWhile {_<Max} filter isCircular
+  val circularPrimes = primes takeWhile {_ < Max} filter isCircular
   def run(args: Array[String]): Unit = {
     println(circularPrimes length)
   }
