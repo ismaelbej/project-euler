2aa0c89871f9bbf20ee89a0fc852ff0907b6f298
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-27 02:45:01 +0000

    Problema 25: Corrijo estilo.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@102 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P25.scala
+++ src/ismael/P25.scala
@@ -1,19 +1,21 @@
 package ismael
 
 object P25 {
-	val digits = 1000
-	def sumFib(p: ((Int, Double), (Int, Double))): (Int, Double) = {
-		val x = p._1
-		val y = p._2
-		if (x._1 == y._1) {
-			val s = x._2 + y._2
-			if (s >= 10.0) (x._1 + 1, s/10.0)
-			else (x._1, s)
-		}
-		else (y._1, x._2/10.0 + y._2)
-	}
-	val fibDigits: Stream[(Int, Double)] = (1, 1.0) #:: (1, 1.0) #:: fibDigits.zip(fibDigits.tail).map(sumFib)
-	def run(args: Array[String]): Unit = {
-		println(1 + (fibDigits takeWhile {_._1 < digits} length))
-	}
+  val digits = 1000
+  def makeFibonacci(p: ((Int, Double), (Int, Double))): (Int, Double) = p match {
+    case (x, y) => {
+      if (x._1 == y._1) {
+        val s = x._2 + y._2
+        if (s >= 10.0) (x._1 + 1, s / 10.0)
+        else (x._1, s)
+      }
+      else (y._1, x._2 / 10.0 + y._2)
+    }
+  }
+  val fibonacciWithDigits: Stream[(Int, Double)] = (1, 1.0) #:: (1, 1.0) #:: fibonacciWithDigits.zip(fibonacciWithDigits.tail).map(makeFibonacci)
+  val fibonacciDigits = fibonacciWithDigits map {_._1}
+  val count = 1 + (fibonacciDigits takeWhile {_ < digits} size)
+  def run(args: Array[String]): Unit = {
+    println(count)
+  }
 }
