3ffee56376940061aee64f2491d5343800c8d1ed
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-09-27 03:41:09 +0000

    Problema 33: * Correccion de estilo. * Uso de las primitivas de la libreria.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@106 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P33.scala
+++ src/ismael/P33.scala
@@ -1,22 +1,20 @@
 package ismael
 
+import ProjectEuler._
+
 object P33 {
-	def isEquivFrac(a: Int, b: Int, d: Int): Boolean = {
-		val ab = 10*a+b
-		val bd = 10*b+d
-		ab*d == bd*a
-	}
-	def isNonTrivialFrac(a: Int, d:Int): Boolean = {
-		!(for (b <- 1 to 9 if isEquivFrac(a, b, d)) yield b).isEmpty
-	}
-	def dcm(n: Int, d: Int) : Int = {
-		if (d==0) n
-		else dcm(d, n%d)
-	}
-	def run(args: Array[String]): Unit = {
-		val frac = for (a <- 1 to 8; d <- a+1 to 9 if isNonTrivialFrac(a, d)) yield (a,d)
-		val (num, denom) = ((1,1) /: frac) {(x, y) => (x._1* y._1, x._2*y._2)}
-		val d = dcm(num, denom)
-		println(denom/d)
-	}
+  def isEquivFrac(a: Int, b: Int, d: Int): Boolean = {
+    val ab = 10 * a + b
+    val bd = 10 * b + d
+    ab * d == bd * a
+  }
+  def isNonTrivialFrac(a: Int, d:Int): Boolean = {
+    1 to 9 exists {x => isEquivFrac(a, x, d)}
+  }
+  def run(args: Array[String]): Unit = {
+    val frac = for (a <- 1 to 8; d <- a + 1 to 9 if isNonTrivialFrac(a, d)) yield (a, d)
+    val (num, denom) = ((1, 1) /: frac) {(x, y) => (x._1 * y._1, x._2 * y._2)}
+    val d = dcm(num, denom)
+    println(denom / d)
+  }
 }
