3ede0dfee7103e7ca66dd1e0cfae6522d86c9ed6
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-10-03 20:38:48 +0000

    Problema 72: Optimizo la funcion calcPhi.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@113 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P72.scala
+++ src/ismael/P72.scala
@@ -4,24 +4,21 @@ import ProjectEuler._
 
 object P72 {
   def calcPhi(n: Int): Long = {
-    def countDiv(m: Int, d: Int): (Int, Int) = {
-      def countDivRec(p: Int, m: Int): (Int, Int) = {
-        if (m % d != 0) (p, m)
-        else countDivRec(p * d, m / d)
+    def reduce(m: Int, d: Int): Int = {
+      def reduceRec(m: Int): Int = {
+        if (m % d != 0) m
+        else reduceRec(m / d)
       }
-      if (m % d == 0) countDivRec(d - 1, m / d)
-      else (1, m)
+      reduceRec(m)
     }
     def calcPhiRec(p: Int, m: Int, q: Stream[Int]): Int = {
       val d = q.head
       if (m == 1) p
-      else if (d*d > m) p * (m - 1)
-      else {
-        val (r, mm) = countDiv(m, d)
-        calcPhiRec(p * r, mm, q.tail)
-      }
+      else if (d*d > m) p / m * (m - 1)
+      else if (m % d != 0) calcPhiRec(p, m, q.tail)
+      else calcPhiRec(p / d * (d - 1), reduce(m, d), q.tail)
     }
-    calcPhiRec(1, n, primes)
+    calcPhiRec(n, n, primes)
   }
   val Max = 1000000
   val reducecFractions = 2 to Max map calcPhi
