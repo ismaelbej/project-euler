c698b7ae5b60bc8f7681d36d4d3229ecc17caeff
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-10-04 03:50:34 +0000

    Problema 1: Reformateo y actualizo para scala 2.8.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@118 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P1.scala
+++ src/ismael/P1.scala
@@ -1,10 +1,9 @@
 package ismael
 
 object P1 {
-	def genList(N : Int) : List[Int] = List.range(1, N) filter {x => x%3==0 || x%5==0}
-
-	def run(args: Array[String]): Unit = {
-		println((0 /: genList(1000)) {_ + _})
-	}
+  val N = 1000
+  val filteredList = 1 to N - 1 filter {x => x % 3 == 0 || x % 5 == 0}
+  def run(args: Array[String]): Unit = {
+    println(filteredList sum)
+  }
 }
-
