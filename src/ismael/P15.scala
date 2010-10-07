f1e5385aca5716da29bae45a9d5f03c4594d6988
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-10-07 01:50:57 +0000

    Problema 15: Corrijo estilo.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@123 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P15.scala
+++ src/ismael/P15.scala
@@ -1,10 +1,9 @@
 package ismael
 
 object P15 {
-	def prod(a: Int, b: Int): BigInt = (BigInt(1) /: List.range(a, b+1)) {_*_}
-	def comb(n: Int, d: Int): BigInt = prod(d+1, n)/prod(1, d)
-	
-	def run(args: Array[String]): Unit = {
-		println(comb(40, 20))
-	}
+  def prod(a: Int, b: Int): BigInt = (BigInt(1) /: List.range(a, b + 1)) {_ * _}
+  def comb(n: Int, d: Int): BigInt = prod(d + 1, n) / prod(1, d)
+  def run(args: Array[String]): Unit = {
+    println(comb(40, 20))
+  }
 }
