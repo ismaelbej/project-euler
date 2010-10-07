abe92b897b8a590b6728f8c8a3483f088db835e9
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-10-07 01:47:48 +0000

    Problema 14: Corrijo el estilo.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@122 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P14.scala
+++ src/ismael/P14.scala
@@ -1,30 +1,28 @@
 package ismael
 
-
 object P14 {
-	val N = 1000000
-	import scala.collection.mutable
-	val values = mutable.Map.empty[Int, Int]
-	def collatz(n: Int): Int = {
-		def collatzRec(n: Long, c: Int): Int = {
-			if (values.contains(n.toInt)) c+values(n.toInt)
-			else
-			{
-				if (n == 1) c
-				else if (n%2 == 0) collatzRec(n/2, c+1)
-				else collatzRec(3*n+1,c+1)
-			}
-		}
-		val r = collatzRec(n, 0)
-		values + ((n, r))
-		r
-	}
-	def run(args: Array[String]): Unit = {
-		val s = (1 to N) map {x => (collatz(x), x)}
-		val m = s.foldLeft((0, 0))((a, b) => {
-			if (b._1 > a._1) b
-			else a
-		})
-		println(m._2)
-	}
+  val N = 1000000
+  import scala.collection.mutable
+  val values = mutable.Map.empty[Int, Int]
+  def collatz(n: Int): Int = {
+    def collatzRec(n: Long, c: Int): Int = {
+      if (values.contains(n.toInt)) c + values(n.toInt)
+      else {
+	if (n == 1) c
+	else if (n % 2 == 0) collatzRec(n / 2, c + 1)
+	else collatzRec(3 * n + 1, c + 1)
+      }
+    }
+    val r = collatzRec(n, 0)
+    values + ((n, r))
+    r
+  }
+  def run(args: Array[String]): Unit = {
+    val s = (1 to N) map {x => (collatz(x), x)}
+    val m = s.foldLeft((0, 0))((a, b) => {
+      if (b._1 > a._1) b
+      else a
+    })
+    println(m._2)
+  }
 }
