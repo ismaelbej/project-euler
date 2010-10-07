defeae3a90d39c45c3a494a05b7ebf62071d21f3
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-10-07 01:51:46 +0000

    Problema 16: Corrijo estilo.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@124 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P16.scala
+++ src/ismael/P16.scala
@@ -1,16 +1,17 @@
 package ismael
 
+import ProjectEuler._
+
 object P16 {
-	val number = power(BigInt(2), 1000)
-	def power(a: BigInt, n: Int): BigInt = (BigInt(1) /: List.fill(n)(a)) {_*_}
-	def sumDigits(n: BigInt): BigInt = {
-		def sumDigitsRec(s: BigInt, n: BigInt): BigInt = {
-			if (n == 0) s
-			else sumDigitsRec(s+n%10, n/10)
-		}
-		sumDigitsRec(0, n)
-	}
-	def run(args: Array[String]): Unit = {
-		println(sumDigits(number))
-	}
+  val number = power(BigInt(2), 1000)
+  def sumDigits(n: BigInt): BigInt = {
+    def sumDigitsRec(s: BigInt, n: BigInt): BigInt = {
+      if (n == 0) s
+      else sumDigitsRec(s + n % 10, n / 10)
+    }
+    sumDigitsRec(0, n)
+  }
+  def run(args: Array[String]): Unit = {
+    println(sumDigits(number))
+  }
 }
