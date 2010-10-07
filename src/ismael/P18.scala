aaa1dcf9c5939e8be3e06dbc90f34fd1136ab44a
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-10-07 01:53:11 +0000

    Problema 18: Corrijo estilo.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@125 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P18.scala
+++ src/ismael/P18.scala
@@ -1,7 +1,7 @@
 package ismael
 
 object P18 {
-	val pyramidString = """75
+  val pyramidString = """75
 95 64
 17 47 82
 18 35 87 10
@@ -16,24 +16,24 @@ object P18 {
 91 71 52 38 17 14 91 43 58 50 27 29 48
 63 66 04 68 89 53 67 30 73 16 69 87 40 31
 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
-	def makePyramid(s: String): Array[Array[Int]] = s.split('\n') map {_.split(' ') map {_.toInt}}
-	val pyramid = makePyramid(pyramidString)
-	def sumPos(t: Array[Int], b: Array[Int], n: Int): Int = {
-		if (n == 1) b(n-1)+t(0)
-		else if (n == b.length) b(n-1)+t(n-2)
-		else b(n-1)+List(t(n-2), t(n-1)).max
-	}
-	def addRows(t: Array[Int], b: Array[Int]): Array[Int] = {
-		(for (n <- 1 to b.length) yield sumPos(t, b, n)) toArray
-	}
-	def findMaxPyramid(a: Array[Array[Int]]): Int = {
-		def addPyramidRows(h: Array[Int], a: Array[Array[Int]]): Array[Int] = {
-			if (a.isEmpty) h
-			else addPyramidRows(addRows(h, a.head), a.tail)
-		}
-		addPyramidRows(a head, a tail) max
-	}
-	def run(args: Array[String]): Unit = {
-		println(findMaxPyramid(pyramid))
-	}
+  def makePyramid(s: String): Array[Array[Int]] = s.split('\n') map {_.split(' ') map {_.toInt}}
+  val pyramid = makePyramid(pyramidString)
+  def sumPos(t: Array[Int], b: Array[Int], n: Int): Int = {
+    if (n == 1) b(n - 1) + t(0)
+    else if (n == b.length) b(n - 1) + t(n - 2)
+    else b(n - 1) + List(t(n - 2), t(n - 1)).max
+  }
+  def addRows(t: Array[Int], b: Array[Int]): Array[Int] = {
+    (for (n <- 1 to b.length) yield sumPos(t, b, n)) toArray
+  }
+  def findMaxPyramid(a: Array[Array[Int]]): Int = {
+    def addPyramidRows(h: Array[Int], a: Array[Array[Int]]): Array[Int] = {
+      if (a.isEmpty) h
+      else addPyramidRows(addRows(h, a.head), a.tail)
+    }
+    addPyramidRows(a head, a tail) max
+  }
+  def run(args: Array[String]): Unit = {
+    println(findMaxPyramid(pyramid))
+  }
 }
