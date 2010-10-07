11ed500c54462fc47bb6d8f57e529f3ffa275017
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-10-07 01:46:16 +0000

    Problema 12: Agrego espacio entre operadores.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@121 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P12.scala
+++ src/ismael/P12.scala
@@ -5,15 +5,15 @@ import ProjectEuler._
 object P12 {
   def reduceDiv(n: Int, d: Int): (Int, Int) = {
     def reduceDivRec(n: Int, c: Int): (Int, Int) = {
-      if (n%d != 0) (c, n)
-      else reduceDivRec(n/d, c+1)
+      if (n % d != 0) (c, n)
+      else reduceDivRec(n / d, c + 1)
     }
     reduceDivRec(n, 0)
   }
   def countDiv(n: Int): Int = {
     def countDivRec(n: Int, p: Int, d: Int): Int = {
       if (n == 1) p
-      else if (d*d > n) 2*p
+      else if (d * d > n) 2 * p
       else {
 	val (c, m) = reduceDiv(n, d)
 	countDivRec(m, p * (c + 1), d + 1)
@@ -22,7 +22,7 @@ object P12 {
     countDivRec(n, 1, 2)
   }
   def countTriangleNumberDiv(n: Int): Int = {
-    if ((n&1) == 0) {
+    if ((n & 1) == 0) {
       countDiv(n / 2) * countDiv(n + 1)
     }
     else {
