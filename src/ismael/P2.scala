f262ec5b9309bf284ebe6cb41b54b15c948918ff
ismael <ismael@8898f3a5-19f9-486b-9892-a321095f26e3>
2010-10-07 02:00:24 +0000

    Problema 2: Corrijo estilo.
    git-svn-id: svn+ssh://quain.asterion/repos/ProjectEuler@126 8898f3a5-19f9-486b-9892-a321095f26e3

--- src/ismael/P2.scala
+++ src/ismael/P2.scala
@@ -1,10 +1,10 @@
 package ismael
 
 object P2 {
-	val Max = 4000000
-	val fib: Stream[Int] = 1 #:: 1 #:: fib.zip(fib.tail).map(p => p._1 + p._2)
-	val evenFib = fib takeWhile {_<Max} filter {_%2 == 0}
-	def run(args: Array[String]): Unit = {
-		println((0 /: evenFib) {_ + _})
-	}
+  val Max = 4000000
+  val fib: Stream[Int] = 1 #:: 1 #:: fib.zip(fib.tail).map(p => p._1 + p._2)
+  val evenFib = fib takeWhile {_ < Max} filter {_ % 2 == 0}
+  def run(args: Array[String]): Unit = {
+    println((0 /: evenFib) {_ + _})
+  }
 }
